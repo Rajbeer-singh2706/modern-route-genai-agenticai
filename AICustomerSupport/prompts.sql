64023ed6f58a7e0d8a516508943bb619ce97e8a9
rajbeer <rajbeer.singh>
2026-03-25 16:17:58 +0530

    updated

---
+++ AICustomerSupport/prompts.sql
@@ -0,0 +1,20 @@
+
+
+## can u explain this project 
+
+### Prompt 1 ##
+I want to build a langGraph-based customer support email agent in Python 
+create only 
+
+
+
+### PROMPT 2 #####
+
+
+
+
+
+
+## PROMPT #######
+can you also create a APU and ui where we can test the system using a mock emali?
+And please create another page where we can view all inbox emails along with their email details and the corresping agent output 
