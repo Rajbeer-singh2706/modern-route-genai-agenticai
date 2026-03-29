0306a0c207bbcb7b5657e2b31891691e1ce85f23
rajbeer <rajbeer.singh>
2026-03-29 22:28:27 +0530

    up

---
+++ UDS20_modenRoute/url_shared.sql
@@ -0,0 +1,7 @@
+# GOITHIUB 
+https://github.com/sunnysavita10/Full-Stack-GenAI-Bootcamp-1.0/tree/main
+
+## NOtion : 
+https://www.notion.so/Class-01-Intro-GenAI-LLMs-331893f0ead0801faf10cde239077308
+
+
